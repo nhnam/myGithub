5d1b757b6f865556d62e6e4efccce403b4e19152
ナム Nam Nguyen <namnh@dgroup.co>
2017-01-28 14:32:25 +0700

    Update

--- myGithub/RepoSectionController.swift
+++ myGithub/RepoSectionController.swift
@@ -42,9 +42,8 @@ extension RepoSectionController: IGListSectionType {
     }
     
     func didSelectItem(at index: Int) {
-        print("Selected \(repository.name)")
         let viewController = FastViewController()
-        //viewController.view.backgroundColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
+        viewController.title = repository.name
         self.viewController?.navigationController?.pushViewController(viewController, animated: true)
     }
 }
--- myGithubTests/myGithubTests.swift
+++ myGithubTests/myGithubTests.swift
@@ -8,7 +8,7 @@
 
 import Quick
 import Nimble
-import IGListKit
+//import IGListKit
 
 class ApiSpec:QuickSpec {
     override func spec() {
@@ -39,6 +39,10 @@ class ApiSpec:QuickSpec {
         describe("Repositories ViewController") {
             let sut = ViewController()
             
+            it("should has title Nam's Github") {
+                let _ = sut.view
+                expect(sut.title).to(equal("Nam's Github"))
+            }
             it("should has collectionView") {
                 let _ = sut.view
                 expect(sut.collectionView).toNot(beNil())
@@ -55,9 +59,38 @@ class ApiSpec:QuickSpec {
         }
         describe("RepositoriesLoader") {
             let loader = RepositoriesLoader()
-            
             it("should has list repositories") {
                 expect(loader.repositories).toNot(beNil())
+            }
+            it("should has data after loadRepos") {
+                waitUntil(timeout: 5.0, action: { (done) in
+                    loader.loadRepos {
+                        expect(loader.repositories.count).to(equal(30))
+                        done()
+                    }
+                })
+            }
+            it("should has data as a list of Repository after app started") {
+                waitUntil(timeout: 5.0, action: { (done) in
+                    loader.loadRepos {
+                        done()
+                    }
+                })
+                let arrRepos = loader.repositories.filter({ (element:Repository) -> Bool in
+                    return(element is Repository)
+                })
+                expect(arrRepos.count).to(equal(loader.repositories.count))
+            }
+            it("should has data as a list of Repository with name, url") {
+                waitUntil(timeout: 5.0, action: { (done) in
+                    loader.loadRepos {
+                        done()
+                    }
+                })
+                let arrRepos = loader.repositories.filter({ (element:Repository) -> Bool in
+                    return(element.name != nil && element.url != nil)
+                })
+                expect(arrRepos.count).to(equal(loader.repositories.count))
             }
         }
     }
